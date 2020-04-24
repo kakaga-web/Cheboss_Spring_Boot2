51ca58e115cd2eff252b7f5df1f3ea2de9345299
Cheboss1 <medchbb@gmail.com>
2020-04-24 11:36:23 +0000

    two files that contain an thorough explanation of exceptions

--- src/main/java/_Java8_exercices/ChebossException/UncheckedExceptionExample.java
+++ src/main/java/_Java8_exercices/ChebossException/UncheckedExceptionExample.java
@@ -15,19 +15,32 @@ public class UncheckedExceptionExample {
         System.out.println("enter the divisor number  : ");
         String divisor = sc2.nextLine();
 
-        try {
+        try { // this is optional, the thrown exception is unchecked
             System.out.println("the remainder is " + divideTheseNumbers(parseInt(dividend), parseInt(divisor)));
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("toString exception method  --> " + e.toString());
             System.out.println("getMessage exception Method --> " + e.getMessage());
+            try {
+                throw new ArithmeticException("Cheboss created an exception that devids by 0 / by zero");
+                // Comment this try catch block and the last two statements after finally won't appear !!!!!
+            } catch (ArithmeticException d) {
+                d.printStackTrace();
+            }
+            // the uncaught Arithmetic exception throw new will prevent the statements after finally from appearing,
+            // because since the exception isn't handled
+            // it's is thrown to the default JVM handler ( so basically it's just finally who will appear
+            // and then BAM! the program is abruptly terminated ( statements after finally won't be shown )
         } finally {
             System.out.println("I'm finally Statement");
         }
+        System.out.println("I'm here because the catch block didn't raise an unhandled exception ( or there is no exception at all ");
         System.out.println("I'm outside all !!!");
     }
 
-    public static int divideTheseNumbers(int dividendArg, int divisorArg) {
+    public static int divideTheseNumbers(int dividendArg, int divisorArg) { // Unchecked exception
         return dividendArg / divisorArg; // Unchecked Exception
     }
+
 }
+
--- src/main/java/_Java8_exercices/ChebossException/chebossCheckedException.java
+++ src/main/java/_Java8_exercices/ChebossException/chebossCheckedException.java
@@ -5,19 +5,21 @@ import java.io.*;
 public class chebossCheckedException {
     public static void main(String[] args) {
 
-        openFile("C:\\Users\\Fatima et yassine\\IdeaProjects\\Cheboss_Spring_Boot2_\\src\\main\\java\\_Java8_exercices\\inputCheboss.txt");
-
-    }
-
-
-    public static void openFile(String name) {
         try {
-            FileInputStream f = new FileInputStream(name);
-
+            openFile("C:\\Users\\Fatima et yassine\\IdeaProjects\\Cheboss_Spring_Boot2_\\src\\main\\java\\_Java8_exercices\\ChebossException\\inputCheboss.txt");
+            System.out.println("Yes the file exists !! ");
         } catch (FileNotFoundException e) {
-            System.out.println("file no found ");
+            System.out.println(" file not found ! ");
             e.printStackTrace();
         }
+
+    }
+// Checked exception Extends Throwable and all the other exceptions are Unchecked exception ( Runtime and Error exceptions )
+
+    public static void openFile(String name) throws FileNotFoundException { // Checked exception
+// I can put a try catch block here so i won't have to declare it in the main method ( i'll also not need throws FileNotFoundException !!
+        FileInputStream f = new FileInputStream(name);
+
     }
 
 
